aad1f02c2188fe7c011902ae193ef54e60c4413b
User <andreytsvl15@gmail.com>
2025-01-09 09:58:31 +0300

    Auto-commit at 2025-01-09 09:58:31

--- store_1/git_auto_commit.py
+++ store_1/git_auto_commit.py
@@ -3,25 +3,38 @@ import logging
 from git import Repo
 from datetime import datetime
 import atexit
+from logging.handlers import RotatingFileHandler
+
 
 # Создаём папку logs, если её нет
 log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
 os.makedirs(log_dir, exist_ok=True)
+
+handler = RotatingFileHandler(
+    os.path.join(log_dir, 'git_auto_commit.log'),
+    maxBytes=1024 * 1024 * 5,  # 5 МБ
+    backupCount=3,  # Хранить 3 файла логов
+)
+
 # Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    filename=os.path.join(log_dir, 'git_auto_commit.log'),  # Логи будут сохраняться в этот файл
-    filemode='a',  # Режим добавления (append)
+    #filename=os.path.join(log_dir, 'git_auto_commit.log'),  # Логи будут сохраняться в этот файл
+    #filemode='a',  # Режим добавления (append)
+    handlers=[handler],
 )
 
 logger = logging.getLogger(__name__)
+
+
 
 # Путь к репозиторию
 repo_path = r'C:\Users\User\Desktop\DiplomProject\DiplomProject'
 
 # Инициализация репозитория
 repo = Repo(repo_path)
+
 
 def git_auto_commit():
     try:
